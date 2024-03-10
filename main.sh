aef31314da7a44584ea58fe521591884a648bcdc
Lemonawa <neko@lma.moe>
2024-03-10 10:38:55 +0800

    do something

--- main.sh
+++ main.sh
@@ -37,7 +37,7 @@ configure(){
 # https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency
 net.ipv4.tcp_rmem = 8192 262144 536870912
 net.ipv4.tcp_wmem = 8192 262144 536870912
-net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_adv_win_scale = 1
 net.ipv4.tcp_collapse_max_bytes = 6291456
 net.ipv4.tcp_notsent_lowat = 131072
 
@@ -51,13 +51,15 @@ net.ipv4.tcp_ecn = 1
 # TCP window scaling, as per RFC1323
 net.ipv4.tcp_window_scaling = 1
 
-# some optimization
-net.ipv4.tcp_keepalive_time = 1800
-net.ipv4.tcp_keepalive_intvl = 30
-net.ipv4.tcp_keepalive_probes = 3
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_tw_recycle = 1
-net.ipv4.tcp_fin_timeout = 30
+# UDP
+net.ipv4.udp_rmem_min=16384
+net.ipv4.udp_wmem_min=16384
+net.core.rmem_default=26214400
+net.core.rmem_max=26214400
+net.core.optmem_max=65535
+net.ipv4.udp_mem=8192 262144 536870912
+net.core.netdev_max_backlog=30000
+
 EOF
     sysctl -p
     clear
